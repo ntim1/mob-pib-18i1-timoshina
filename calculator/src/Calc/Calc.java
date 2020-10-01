ae05b7c453cc316d1181dd4d36cc68be43cb4066
ntimoshina <timnata98@mail.ru>
2020-10-01 16:27:31 +0600

    GIT STILL HARD

--- calculator/src/Calc/Calc.java
+++ calculator/src/Calc/Calc.java
@@ -4,15 +4,14 @@ public class Calc {
 
     public static void main(String[] args) {
         try {
-            // String S = "-1.5 + 1e-2";
-             String S = "95 / 0";
+            String S = "-1.5 + 1e-2";
+            // String S = "95 / 0";
             String[] Args = S.split(" ");
             double num1 = Double.parseDouble(Args[0]);
             char operation = Args[1].charAt(0);
             double num2 = Double.parseDouble(Args[2]);
             System.out.println("Solution: " + calc(num1, num2, operation));
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
     }
@@ -28,8 +27,7 @@ public class Calc {
             case '/':
                 if (num2 == 0) {
                     System.out.println("Error!!!!");
-                }
-                else return num1 / num2;
+                } else return num1 / num2;
             default:
                 return Double.NaN;
         }
